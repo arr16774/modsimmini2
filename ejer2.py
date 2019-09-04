94e39366a883cea50d1335cf75129ec434afee01
Unknown <jua16073@uvg.edu.gt>
2019-09-04 17:03:43 -0600

    ejer 2 con funcion normal

--- ejer2.py
+++ ejer2.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import random
 
@@ -10,6 +10,21 @@ def gen(p, f):
     return acumulado
   return func
 
+def normalapply():
+  return normal(-600, 50)
+
+def normal(mu, sigma):
+    while True:
+        y1 = np.log(1 - random.random())/-1
+        y2 = np.log(1 - random.random())/-1
+        if (y2 - (y1-1) ** 2) /2 > 0:
+            y1 = y2 - ((y1-1) ** 2) / 2
+            rand = random.random()
+            if rand < 0.5:
+                return mu + sigma * y1
+            else:
+                return mu - sigma * y1
+
 def main(iterations, p, f):
   f = gen(p, f)
   p = []
@@ -20,4 +35,4 @@ def main(iterations, p, f):
   plt.show()
 
 if __name__ == "__main__":
-  main(100000, [1], [random.random])
+  main(100000, [0.5, 0.5], [random.random, normalapply])
